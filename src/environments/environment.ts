6853a042994137c1f8bcd63b371f5fb41cd1ddcb
salahSurvivor <salaheddinenaji2000@gmail.com>
2025-11-06 16:51:50 +0100

    Maj cv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://portfolio-backend-x5yn.onrender.com'
-  // apiUrl: 'http://localhost:3000'
+ // apiUrl: 'http://localhost:3000'
 };
